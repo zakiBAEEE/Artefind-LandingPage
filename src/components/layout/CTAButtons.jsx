7bdf474e43bae0e729cafb1cd447bbc11ab79669
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-11-24 16:36:40 +0700

    Update

--- src/components/layout/CTAButtons.jsx
+++ src/components/layout/CTAButtons.jsx
@@ -5,7 +5,7 @@ function CTAButtons() {
     return (
         <div className="flex flex-col sm:flex-row gap-5 w-3/4 md:w-full sm:w-auto">
             {/* Primary Button: Cream Background, Dark Text */}
-            <a className="group relative px-8 py-4 bg-[rgb(255,248,212)] text-[rgb(49,54,71)] rounded-xl font-bold shadow-[0_0_15px_rgba(255,248,212,0.3)] hover:shadow-[0_0_25px_rgba(255,248,212,0.5)] hover:translate-y-[2px] transition-all overflow-hidden flex justify-center items-center" href="https://drive.google.com/drive/folders/1J2VjUsMYbZU7LMouw94MhgqnE3OMQveS?usp=sharing">
+            <a className="group relative px-8 py-4 bg-[rgb(255,248,212)] text-[rgb(49,54,71)] rounded-xl font-bold shadow-[0_0_15px_rgba(255,248,212,0.3)] hover:shadow-[0_0_25px_rgba(255,248,212,0.5)] hover:translate-y-[2px] transition-all overflow-hidden flex justify-center items-center" href="https://drive.google.com/drive/folders/1J2VjUsMYbZU7LMouw94MhgqnE3OMQveS?usp=sharing" target="_blank">
                 <span className="absolute top-0 left-0 w-full h-full bg-white/50 transform -skew-x-12 -translate-x-full group-hover:animate-shine"></span>
                 <span className="relative flex items-center gap-3 font-nusantara text-xl">
                     <Download size={24} fill="currentColor" className="text-[rgb(49,54,71)]" /> Download Sekarang
